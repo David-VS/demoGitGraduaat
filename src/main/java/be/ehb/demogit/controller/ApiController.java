3e68731caad169d5c937b70eb95555ff527828c4
David-VS <63110701+David-VS@users.noreply.github.com>
2024-04-26 14:46:36 +0200

    created route /hello

--- src/main/java/be/ehb/demogit/controller/ApiController.java
+++ src/main/java/be/ehb/demogit/controller/ApiController.java
@@ -1,4 +1,15 @@
 package be.ehb.demogit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
 }
